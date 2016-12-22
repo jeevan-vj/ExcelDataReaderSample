67c7f9229f9a562a97dce89d461971bc95581dc9
jeevan wijerathna <jeevan90wijerathna@outlook.com>
2016-12-22 15:08:37 +0530

    initial

---
+++ ExcelReading/Form1.cs
@@ -0,0 +1,49 @@
+﻿using Excel;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ExcelReading
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string filePath = "Book1.xlsx";
+            FileStream stream = File.Open(filePath, FileMode.Open, FileAccess.Read);
+
+            //Choose one of either 1 or 2
+            //1. Reading from a binary Excel file ('97-2003 format; *.xls)
+          //  IExcelDataReader excelReader = ExcelReaderFactory.CreateBinaryReader(stream);
+
+            //2. Reading from a OpenXml Excel file (2007 format; *.xlsx)
+            IExcelDataReader excelReader = ExcelReaderFactory.CreateOpenXmlReader(stream);
+
+            //Choose one of either 3, 4, or 5
+            //3. DataSet - The result of each spreadsheet will be created in the result.Tables
+           // DataSet result = excelReader.AsDataSet();
+
+            //4. DataSet - Create column names from first row
+            excelReader.IsFirstRowAsColumnNames = true;
+            DataSet result = excelReader.AsDataSet();
+
+            dataGridView1.DataSource = result.Tables[0]; 
+            
+
+            //6. Free resources (IExcelDataReader is IDisposable)
+            excelReader.Close();
+        }
+    }
+}
